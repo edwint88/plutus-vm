f184ff60fab3dcaff0b6cbed7d37d3d54302e44f
edwint88 <tuzaredwin@gmail.com>
2021-04-22 01:46:26 +0200

    add script to update Plutus Playground for a running VM

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Vagrant + VBox for plutus playground
 8. Go in your browser at https://192.168.5.21:8009 (if you changed the VM IP go to that IP)
 9. How to SSH-Remote: https://medium.com/@lopezgand/connect-visual-studio-code-with-vagrant-in-your-local-machine-24903fb4a9de
 
-
+## Update and recompile Plutus-playground for a running VM 
+Go to `cd /home/vagrant/plutus/git/plutus` => run `git pull` => run update `/bin/bash ../updatePlutus.sh`
 ## Tips & Tricks
 1. If you need ghc or cabal you can enter a folder with `default.nix` and run `nix-shell` over there and then change to your directory where you need to run cabal. E.g. `cd /home/vagrant/plutus/git/plutus` => `nix-shell` => `cd ../plutus-pioneer-program/code/week1` => `cabal build`
--- vagrant/ubuntu/prepare-plutus-2.sh
+++ vagrant/ubuntu/prepare-plutus-2.sh
@@ -8,14 +8,16 @@ cd /home/$1/plutus/git
 git clone https://github.com/input-output-hk/plutus
 git clone https://github.com/input-output-hk/plutus-starter.git
 git clone https://github.com/input-output-hk/plutus-pioneer-program.git
+touch updatePlutus.sh
+echo "nix build -f default.nix plutus.haskell.packages.plutus-core.components.library" >> updatePlutus.sh
+echo "nix-build -A plutus-playground.client" >> updatePlutus.sh
+echo "nix-build -A plutus-playground.server" >> updatePlutus.sh
+echo "nix-build -A plutus-playground.generate-purescript" >> updatePlutus.sh
+echo "nix-build -A plutus-playground.start-backend" >> updatePlutus.sh
+echo "nix-build -A plutus-pab" >> updatePlutus.sh
+echo "nix-shell --command \"cd plutus-pab; plutus-playground-generate-purs; cd ..; cd plutus-playground-server; plutus-playground-generate-purs\"" >> updatePlutus.sh
 cd ./plutus
-nix build -f default.nix plutus.haskell.packages.plutus-core.components.library
-nix-build -A plutus-playground.client
-nix-build -A plutus-playground.server
-nix-build -A plutus-playground.generate-purescript
-nix-build -A plutus-playground.start-backend
-nix-build -A plutus-pab
-nix-shell --command "cd plutus-pab; plutus-playground-generate-purs; cd ..; cd plutus-playground-server; plutus-playground-generate-purs"
+/bin/bash ../updatePlutus.sh
 # Add host 0.0.0.0 -> to access the frontend in browser 192.168.5.x
 cp plutus-playground-client/package.json plutus-playground-client/package.json.bak 
 sed 's/--mode=development/--mode=development --host 0.0.0.0/g' plutus-playground-client/package.json.bak > plutus-playground-client/package.json
