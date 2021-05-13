3528ba2fb9aa30da39fc73647cab3f0faf56a73f
edwint88 <tuzaredwin@gmail.com>
2021-05-13 20:11:36 +0200

    fix file provisioning

--- vagrant/ubuntu/prepare-plutus-2.sh
+++ vagrant/ubuntu/prepare-plutus-2.sh
@@ -7,7 +7,7 @@ mkdir -p /home/$1/plutus/git
 # Prepare VIM
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +qa
+vim +'PlugInstall --sync' +qa 2>&1
 
 cd /home/$1/plutus/git
 git clone https://github.com/input-output-hk/plutus
